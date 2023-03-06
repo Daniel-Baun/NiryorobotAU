5cbe96a47aa85a4f4cfa887adfe84b0388f9f7ee
Daniel Baun Olesen <bdaniel@live.dk>
2023-03-06 12:52:20 +0100

    robot picks any object and places on conveyor belt

--- test.py
+++ test.py
@@ -15,25 +15,24 @@ robot1.calibrate_auto()
 # Move joints
 #robot.move_joints(0.0, 0.151, -0.487, 0.047, 0.000, -0.129)
 robot1.move_joints(position_storage_observer_robot1)
+robot1.wait(3)
 print(robot1.get_workspace_list())
 obj_found, shape_ret, color_ret = robot1.vision_pick('STORAGE_1_WORKSPACE',
                                                     height_offset=0.0,
-                                                    shape=ObjectShape.SQUARE,
+                                                    shape=ObjectShape.CIRCLE,
                                                     color=ObjectColor.RED)
 
 robot1.move_joints(position_over_con1_robot1)
-robot1.open_gripper(1000)
-
+robot1.release_with_tool()
+robot1.grasp_with_tool()
 #start conveyer belt 1
-conveyer_id = robot1.set_conveyor()
-robot1.run_conveyor(conveyer_id, speed=50, direction=ConveyorDirection.FORWARD)
+conveyor_id_1 = robot1.set_conveyor()
+robot1.run_conveyor(conveyor_id_1, speed=100, direction=ConveyorDirection.FORWARD)
 
-robot1.wait(5)
-
+robot1.wait(7.5)
+robot1.stop_conveyor(conveyor_id_1)
 
 
 robot1.move_to_home_pose()
-# Turn learning mode ON
-robot1.set_learning_mode(True)
 # Stop TCP connection
 robot1.close_connection()
