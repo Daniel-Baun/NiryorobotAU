60c6289b9641b2abf2bfb31fdacb14ad171d0819
Daniel Baun Olesen <bdaniel@live.dk>
2023-03-28 09:31:43 +0200

    added variable quantity fetching sql

--- odoo.py
+++ odoo.py
@@ -7,19 +7,32 @@ connection = psycopg2.connect(database = "main_db", user = "au682915", password 
 #    
 #
 #    return connection
-
 product_chosen = 'GR01'
 
 cur = connection.cursor()
 
-cur.execute("SELECT public.stock_quant.quantity, public.product_product.id "+ 
+def check_connection(database: str, user: str, password: str, host: str, port: str):
+    try:
+        conn = psycopg2.connect(database = database, user = user, password = password, host = host, port = port, connect_timeout=1)
+        conn.close()
+        return True
+    except psycopg2.Error:
+        return False
+
+
+def get_quantity_product(product: str, cursor):
+
+    query = ("SELECT public.stock_quant.quantity, public.product_product.id "+ 
             "FROM public.stock_quant " + 
             "JOIN public.product_product ON public.stock_quant.product_id = public.product_product.id "+
-            "WHERE public.product_product.default_code = 'GR01' AND public.stock_quant.quantity > -1")
+            "WHERE public.product_product.default_code = %s AND public.stock_quant.quantity > -1")
+    cursor.execute(query, (product,))
+    
+    return cursor.fetchone()
 
-rows = cur.fetchall()
+def change_quantity_product():
 
-for row in rows:
-    print(row)
+    
+print(get_quantity_product('GR01', cur))
 
 connection.close()  
