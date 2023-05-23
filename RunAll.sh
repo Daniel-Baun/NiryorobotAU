e71edbe0f8999a549a486a9e38093b5c8b8fdc49
Daniel Baun Olesen <bdaniel@live.dk>
2023-05-23 10:34:23 +0200

    semi working bash

--- RunAll.sh
+++ RunAll.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 cd ../Odoo/odoo &&
-x-terminal-emulator -e python3 odoo-bin -d main_db &&
-cd ../../Code/Python &&
-python3 GUI.py
+nohup python3 odoo-bin -d main_db  > odoo_log.txt 2>&1 &
+sleep 3
 
+current_dir=$(pwd)
+echo "Current dir: $current_dir"
+cd Python &&
+nohup python3 GUI.py
+nohup python3 robots.py
+
+
+cd ../FMU/Maestro &&
+nohup java -jar maestro-2.3.0-jar-with-dependencies.jar interpret import/spec.mabl -output output
+
+cd ../../Python &&
+nohup python3 publish_to_fmu.py
 
